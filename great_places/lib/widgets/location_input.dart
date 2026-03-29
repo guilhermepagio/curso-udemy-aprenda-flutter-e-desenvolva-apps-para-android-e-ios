473c4bcdef3b00d557d409fda6d6ae67b922a67f
guilhermepagio <guilhermepagio.contato@gmail.com>
2026-03-29 01:22:49 -0300

    feat: secao 13 29/03/2026 01:22

---
+++ great_places/lib/widgets/location_input.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _previewImageUrl == null
+              ? Text('Nenhuma Localização Informada')
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.location_on),
+              label: Text('Localização Atual'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.map),
+              label: Text('Selecione no Mapa'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
